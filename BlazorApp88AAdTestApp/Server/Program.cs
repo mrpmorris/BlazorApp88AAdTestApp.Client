71061e4c3f9e18e23d11ca2ed2e3ec28a45bcf65
Camilo Terevinto <camiloterevinto@outlook.com>
2023-02-17 21:30:43 +0000

    Get rid of all manual configuration for AAD auth and simplify Program class.

--- BlazorApp88AAdTestApp/Server/Program.cs
+++ BlazorApp88AAdTestApp/Server/Program.cs
@@ -1,118 +1,54 @@
 using BlazorApp88AAdTestApp.Server;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Logging;
-using Microsoft.IdentityModel.Protocols;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.IdentityModel.Tokens;
-
-namespace BlazorApp88AAdTestApp
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      var builder = WebApplication.CreateBuilder(args);
-
-      builder.Services.AddCors(options =>
-      {
-        options.AddPolicy(name: "eggs", builder => builder
-        .AllowAnyOrigin()
-        .AllowAnyHeader()
-        .AllowAnyMethod());
-      });
+using Microsoft.Identity.Web;
 
 #if DEBUG
-      IdentityModelEventSource.ShowPII = true;
+Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
 #endif
-      //builder.Services
-      //	.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration, "AzureAd")
-      //	.EnableTokenAcquisitionToCallDownstreamApi()
-      //	.AddInMemoryTokenCaches();
-      builder.Services
-        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(async options =>
-        {
-          var originalEvents = options.Events;
-          options.Events = new JwtBearerEvents()
-          {
-            OnAuthenticationFailed = context =>
-            {
-              return originalEvents?.OnAuthenticationFailed(context) ?? Task.CompletedTask;
-            },
-            OnChallenge = context =>
-            {
-              return originalEvents?.OnChallenge(context) ?? Task.CompletedTask;
-            },
-            OnForbidden = context =>
-            {
-              return originalEvents?.OnForbidden(context) ?? Task.CompletedTask;
-            },
-            OnMessageReceived = context =>
-            {
-              return originalEvents?.OnMessageReceived(context) ?? Task.CompletedTask;
-            },
-            OnTokenValidated = context =>
-            {
-              return originalEvents?.OnTokenValidated(context) ?? Task.CompletedTask;
-            }
-          };
-          options.Authority = "https://sts.windows.net/47f62749-2a26-422a-b1ba-f6c1d8d66eb3";
-          options.Audience = "api://8244fb6c-364c-4762-a078-70880972f7f0";
 
-          string metadataUrl = $"{options.Authority}/.well-known/openid-configuration";
-          var configManager = new ConfigurationManager<OpenIdConnectConfiguration>(metadataUrl, new OpenIdConnectConfigurationRetriever());
-					OpenIdConnectConfiguration config = await configManager.GetConfigurationAsync();
-					ICollection<SecurityKey> signingKeys = config.SigningKeys;
+var builder = WebApplication.CreateBuilder(args);
 
-          options.TokenValidationParameters = new TokenValidationParameters
-          {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKeys = signingKeys,
-            ValidateIssuer = true,
-            ValidIssuer = "https://sts.windows.net/47f62749-2a26-422a-b1ba-f6c1d8d66eb3/",
-            ValidateAudience = true,
-            ValidAudience = "api://8244fb6c-364c-4762-a078-70880972f7f0",
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero
-          };
-        });
-        //.AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-      builder.Services.AddAuthorization();
-      builder.Services.AddControllersWithViews();
-      builder.Services.AddRazorPages();
-      builder.Services.AddSingleton<ContentSecurityPolicyMiddleware>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "eggs", builder => builder
+        .WithOrigins("https://localhost:6510") // AllowAnyOrigin is not recommended
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
 
+builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+builder.Services.AddRazorPages();
+builder.Services.AddSingleton<ContentSecurityPolicyMiddleware>();
 
-      var app = builder.Build();
-      app.UseMiddleware<ContentSecurityPolicyMiddleware>();
+var app = builder.Build();
+app.UseMiddleware<ContentSecurityPolicyMiddleware>();
 
-      // Configure the HTTP request pipeline.
-      if (app.Environment.IsDevelopment())
-      {
-        app.UseWebAssemblyDebugging();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-
-      app.UseHttpsRedirection();
-
-      app.UseBlazorFrameworkFiles();
-      app.UseStaticFiles();
-
-      app.UseRouting();
-      app.UseCors("eggs");
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.MapRazorPages();
-      app.MapControllers();
-      app.MapFallbackToFile("index.html");
-
-      app.Run();
-    }
-  }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseCors("eggs");
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
